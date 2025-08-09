e86e5752adc830b5dc942fc0d2ff2f39062ded07
C. M. de Picciotto <d3p1@d3p1.dev>
2025-08-09 19:27:36 +0000

    feat: add table model [#1]

--- src/js/component/App.tsx
+++ src/js/component/App.tsx
@@ -4,16 +4,29 @@
  */
 import {OrbitControls} from '@react-three/drei'
 import {Canvas} from '@react-three/fiber'
+import {Suspense} from 'react'
+import {Table} from './app/Table.tsx'
 
 export const App = () => {
   return (
-    <Canvas camera={{position: [2, 3, 4]}}>
+    <Canvas camera={{position: [2, 3, 4]}} shadows={true}>
       <OrbitControls />
 
-      <mesh>
-        <boxGeometry />
-        <meshNormalMaterial />
+      <ambientLight intensity={1.5} />
+      <directionalLight
+        position={[2, 3, 4]}
+        intensity={4.5}
+        castShadow={true}
+      />
+
+      <mesh receiveShadow={true} rotation-x={-Math.PI / 2} position-y={-1}>
+        <circleGeometry args={[3]} />
+        <meshStandardMaterial color="orange" />
       </mesh>
+
+      <Suspense>
+        <Table />
+      </Suspense>
     </Canvas>
   )
 }
