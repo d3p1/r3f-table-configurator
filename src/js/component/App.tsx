65656fbc7918ffc47cdfca8af264193b179352a1
C. M. de Picciotto <d3p1@d3p1.dev>
2025-08-14 18:25:50 +0000

    feat: add glass effect to menu [#1]

--- src/js/component/App.tsx
+++ src/js/component/App.tsx
@@ -5,37 +5,36 @@
 import {OrbitControls} from '@react-three/drei'
 import {Canvas} from '@react-three/fiber'
 import {Suspense, useState} from 'react'
-import {TABLE_MODEL_TYPE, type TableModel} from '../types'
+import {
+  TABLE_MODEL_COLOR,
+  TABLE_MODEL_TYPE,
+  type TableColor,
+  type TableModel,
+} from '../types'
 import {Table} from './app/Table.tsx'
 
 export const App = () => {
   const [tableModelType, setTableModelType] = useState<TableModel>(
     TABLE_MODEL_TYPE.CLASSIC.value,
   )
+  const [tableModelColor, setTableModelColor] = useState<string>(
+    TABLE_MODEL_COLOR[0],
+  )
 
   return (
     <>
-      <Canvas camera={{position: [2, 3, 4]}} shadows={true}>
-        <OrbitControls />
+      <Canvas camera={{position: [2, 1, 4]}}>
+        <OrbitControls maxPolarAngle={Math.PI / 2} minPolarAngle={0} />
 
-        <ambientLight intensity={1.5} />
-        <directionalLight
-          position={[2, 3, 4]}
-          intensity={4.5}
-          castShadow={true}
-        />
-
-        <mesh receiveShadow={true} rotation-x={-Math.PI / 2} position-y={-1}>
-          <circleGeometry args={[3]} />
-          <meshStandardMaterial color="orange" />
-        </mesh>
+        <ambientLight intensity={2} />
+        <directionalLight position={[2, 1, 4]} intensity={4.5} />
 
         <Suspense>
-          <Table modelType={tableModelType} />
+          <Table modelType={tableModelType} modelColor={tableModelColor} />
         </Suspense>
       </Canvas>
 
-      <div className="fixed right-10 top-10 flex flex-col gap-4 p-8 text-sm rounded-2xl shadow-[black_0_0_1rem] opacity-50 hover:opacity-100 transition-opacity duration-500">
+      <div className="fixed right-10 top-10 flex flex-col gap-4 p-8 text-sm rounded-2xl shadow-[black_0_0_1rem] text-secondary-600 bg-glass-100 backdrop-blur-lg hover:bg-glass-500 hover:text-secondary-900 transition-colors duration-500">
         <div>
           <h3 className="font-black uppercase mb-3">Model</h3>
 
@@ -69,6 +68,28 @@ export const App = () => {
                         .label
                     }
                   </label>
+                </li>
+              )
+            })}
+          </ul>
+        </div>
+
+        <div>
+          <h3 className="font-black uppercase mb-3">Color</h3>
+
+          <ul className="flex flex-col gap-3">
+            {TABLE_MODEL_COLOR.map((color) => {
+              return (
+                <li key={color} className="flex flex-row gap-2 text-xs">
+                  <input
+                    type="radio"
+                    name="table_color"
+                    id={`table_color_${color}`}
+                    value={color}
+                    checked={tableModelColor === color}
+                    onChange={() => setTableModelColor(color)}
+                  />
+                  <label htmlFor={`table_color_${color}`}>{color}</label>
                 </li>
               )
             })}
